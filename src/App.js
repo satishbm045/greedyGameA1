0e88dec5195b468d04a87688974c136b482a00d2
Satish Madawal <satishbm045@gmail.com>
2019-09-16 14:44:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,28 @@ import {GetData} from './reducer/Action/Action'
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+			dates: {
+        fromDate: '',
+        toDate: ''
+      }
+		}
   }
   componentDidMount() {
     this.props.dispatch(GetData())
+  }
+  dateSelect = (fromDate,toDate) =>{
+    this.setState({
+      dates.fromDate : fromDate,
+      dates.toDate : toDate
+    })
   }
   render(){
     return (
       <div className="mainApp">
         <div className="graphBlock">
           { !this.props.apiData.loading && this.props.apiData.posts &&
-            <GraphicalData apiData={this.props.apiData.posts} />
+            <GraphicalData apiData={this.props.apiData.posts} dateSelect={this.dateSelect}/>
           }
         </div>
         <div className="tableBlock">
